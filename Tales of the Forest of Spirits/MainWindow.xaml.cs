055466f0087da9f66298045b82787dfec112d9f1
Csenge Adél <gom.csenge@gmail.com>
2021-12-11 19:03:31 +0100

    Karakter alapok

--- Tales of the Forest of Spirits/MainWindow.xaml.cs
+++ Tales of the Forest of Spirits/MainWindow.xaml.cs
@@ -20,16 +20,101 @@ namespace Tales_of_the_Forest_of_Spirits
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int hp = 0;
+        public bool strenght = false;
+        public bool magic = false;
+        public bool fear = false;
+        public string creature;
+
         public MainWindow()
         {
             InitializeComponent();
+            startsbutton.Visibility = Visibility.Visible;
+            welcomelabel.Visibility = Visibility.Visible;
+            chooselabel.Visibility = Visibility.Collapsed;
+            pixiebutton.Visibility = Visibility.Collapsed;
+            elfbutton.Visibility = Visibility.Collapsed;
+            bansheebutton.Visibility = Visibility.Collapsed;
+            creaturelabel.Visibility = Visibility.Collapsed;
+            hplabel.Visibility = Visibility.Collapsed;
+
+
         }
 
-        private void startsbutton_Click(object sender, RoutedEventArgs e)
+        public void startsbutton_Click(object sender, RoutedEventArgs e)
         {
             startsbutton.Visibility = Visibility.Collapsed;
             welcomelabel.Visibility = Visibility.Collapsed;
+            chooselabel.Visibility = Visibility.Visible;
+            pixiebutton.Visibility = Visibility.Visible;
+            elfbutton.Visibility = Visibility.Visible;
+            bansheebutton.Visibility = Visibility.Visible;
 
+        }
+
+        private void pixiebutton_Click(object sender, RoutedEventArgs e)
+        {
+            hp = 180;
+            strenght = false;
+            magic = true;
+            fear = false;
+            creature = "p";
+            chooselabel.Visibility = Visibility.Collapsed;
+            elfbutton.Visibility = Visibility.Collapsed;
+            bansheebutton.Visibility = Visibility.Collapsed;
+            creaturelabel.Visibility = Visibility.Visible;
+            hplabel.Visibility = Visibility.Visible;
+        }
+
+        private void elfbutton_Click(object sender, RoutedEventArgs e)
+        {
+            hp = 230;
+            strenght = true;
+            magic = false;
+            fear = false;
+            creature = "e";
+            chooselabel.Visibility = Visibility.Collapsed;
+            pixiebutton.Visibility = Visibility.Collapsed;
+            bansheebutton.Visibility = Visibility.Collapsed;
+            creaturelabel.Visibility = Visibility.Visible;
+            hplabel.Visibility = Visibility.Visible;
+        }
+
+        private void bansheebutton_Click(object sender, RoutedEventArgs e)
+        {
+            hp = 300;
+            strenght = false;
+            magic = false;
+            fear = true;
+            creature = "b";
+            chooselabel.Visibility = Visibility.Collapsed;
+            pixiebutton.Visibility = Visibility.Collapsed;
+            elfbutton.Visibility = Visibility.Collapsed;
+            creaturelabel.Visibility = Visibility.Visible;
+            hplabel.Visibility = Visibility.Visible;
+        }
+
+
+        private void hplabel_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            hplabel.Content = "HP: " + hp;
+            
+        }
+
+        private void creaturelabel_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (creature == "p")
+            {
+                creaturelabel.Content = "Your powers are: tricking; flying and vanishing";
+            }
+            else if (creature == "e")
+            {
+                creaturelabel.Content = "Your powers are: bribing, turning into an objetóct and retreating";
+            }
+            else if (creature == "b")
+            {
+                creaturelabel.Content = "Your powers are: screaming, teleportation and staring menacingly";
+            }
         }
     }
 }
